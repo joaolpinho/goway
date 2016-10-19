ea35c7f9ed7302e65f0d4747731324f36874251e
João Pinho <joaolpinho@gmail.com>
2016-10-19 10:40:50 +0100

    feat(handlers)  make use of success || error callbacks for custom handling

--- handlers/jwt/jwt.go
+++ handlers/jwt/jwt.go
@@ -9,8 +9,24 @@ import (
 	"github.com/pkg/errors"
 )
 
-
 var regexpBearer = regexp.MustCompile("^Bearer\\s([A-Za-z0-9\\-\\._~\\+\\/]+=*)$")
+
+
+func opCallback( route *router.Route, r *http.Request, claim *jwt.MapClaims ) bool { return true }
+func errCallback(  route *router.Route, r *http.Request, err error ) bool { return false }
+
+
+func NewJWTHandler( secret string, algorithm *jwt.SigningMethodHMAC ) (*JWTHandler) {
+	return &JWTHandler{
+		Secret: secret,
+		Algorithm: algorithm,
+		OnSuccess: opCallback,
+		OnError: errCallback,
+	}
+}
+func MakeJWTHandler( secret string, algorithm *jwt.SigningMethodHMAC ) (JWTHandler) {
+	return *NewJWTHandler(secret, algorithm)
+}
 
 
 func ( handler *JWTHandler ) validateSignature(token *jwt.Token) (interface{}, error) {
@@ -18,10 +34,10 @@ func ( handler *JWTHandler ) validateSignature(token *jwt.Token) (interface{}, e
 		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 	}
 
-	return []byte(handler.Options.Secret), nil
+	return []byte(handler.Secret), nil
 }
 
-func (handler *JWTHandler) decode(tk string) (jwt.MapClaims, error) {
+func (handler *JWTHandler) decode(tk string) (*jwt.MapClaims, error) {
 
 	var claims jwt.MapClaims
 	var err error
@@ -36,32 +52,31 @@ func (handler *JWTHandler) decode(tk string) (jwt.MapClaims, error) {
 	if (!ok) {
 		return nil, errors.New("Invalid claim.")
 	}
-	return  claims, nil
+	return  &claims, nil
 }
 
-
-
-type JWTOptions struct {
-	Secret string
-	Algorithm *jwt.SigningMethodHMAC
-}
-type JWTHandler struct {
-	Options JWTOptions
-}
 func (handler *JWTHandler) Handle(route *router.Route, r *http.Request)(bool){
 
 	matches := regexpBearer.FindStringSubmatch(r.Header.Get("Authorization"))
+
+
 	if ( len(matches) != 2 ) {
 		return false
 	}
 
-	if claim, err := handler.decode(matches[1]); err != nil {
+	claim, err := handler.decode(matches[1])
+	if (err != nil) {
 		fmt.Errorf("%s", err.Error())
-		return false
-	} else {
-		fmt.Println(claim)
-		return true
+		return handler.OnError( route, r, err  )
 	}
+
+	if err := claim.Valid(); err != nil {
+		fmt.Errorf("%s", err.Error())
+		return handler.OnError( route, r, err  )
+	}
+
+
+	return handler.OnSuccess( route, r, claim )
 }
 
 
