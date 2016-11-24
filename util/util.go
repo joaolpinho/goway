de434a1e0396e59c6a45841423af00b3ad83012b
Andre Pinto <andre40826@gmail.com>
2016-11-24 13:23:14 +0000

    add util fuction: FilterProductRoutesByAssets

--- util/util.go
+++ util/util.go
@@ -47,6 +47,24 @@ func FilterClientRoutesByAssets(cl []product.Client_v1, asset []string, f func(p
 	return clients
 }
 
+func FilterProductRoutesByAssets(cl []product.Product_v1, asset []string, f func(product.Routes_v1, []string) bool) []product.Product_v1 {
+	products := make([]product.Product_v1, 0)
+	for _, c := range cl {
+		routes := make([]product.Routes_v1, 0)
+		for _, v := range c.Routes {
+			if f(v, asset) {
+				routes = append(routes, v)
+			}
+		}
+
+		c.Routes = routes
+		products = append(products, c)
+	}
+
+
+	return products
+}
+
 func FilterByAsset(route product.Routes_v1, assets []string) bool{
 	return Filter(assets, route.Asset)
 }
