3b9628a7df0f06a4b6b8137c05f2489878564dbe
João Pinho <joaolpinho@gmail.com>
2016-12-16 15:28:22 +0000

    chore(router) use inherited func

--- router/goway-client-router.go
+++ router/goway-client-router.go
@@ -31,13 +31,6 @@ func (r *GowayClientRouter) LoadRoutes(clients []product.Client_v1)  {
 	r.GoWayRouter.Compile()
 }
 
-
-func (r *GowayClientRouter) CheckRoute(path string, verb string, code string, version string) (*Route, map[string]interface{})  {
-	route, params := r.GoWayRouter.CheckRoute(path, verb, code, version)
-	return route, params
-}
-
-
 func (r *GowayClientRouter) CheckClient(path string, version string) *product.Client_v1{
 	x:= r.Clients[util.ClientCode(path, version)]
 	return &x
